cafff0d4ea77d422acff3fe8de990db41d2aeb24
ian <zyktnn@gmail.com>
2017-05-14 20:08:54 +0300

    Giant refactor: moved supporting code to RungeKutta.py

--- RungeKutta.py
+++ RungeKutta.py
@@ -1,5 +1,7 @@
-import math
 from collections import namedtuple
+from itertools import zip_longest
+
+import numpy as np
 from numpy.linalg import norm
 
 
@@ -9,60 +11,25 @@ class System:
         self.rhs = right_hand_side
 
     def apply(self, *args):
+        # print(args)
         return [f(*args) for f in self.rhs]
-
-
-class RungeKuttaState:
-    def __init__(self, x, ys, rhs: System, butcher_table: ButcherTable):
-        self.x = x
-        self.ys = ys
-        self.rhs = rhs
-        self.step = 2  # TODO: init step method
-        self.table = butcher_table
-
-    def initial_step(self, start, end, system: System, order):
-        delta = (1 / max(start, end))**(s+1) + norm(system.apply(start))
-
-
-    def correction_h(self, step):
-        ks = []
-        for (c, _as, b) in zip(*self.table):
-            k_ys = [y + sum(a*k for (k, a) in zip(ks, _as)) for y in self.ys]
-            ks.append([step * f(self.x + c*step, *k_ys) for f in self.fs])
-        return ks
-
-    def correction(self):
-        return self.correction_h(self.step)
-
-    def ys_hs(self, step):
-        return [y + k for y, k in zip(self.ys, self.correction_h(step))]
-
-    def next_ys(self):
-        return [y + k for y, k in zip(self.ys, self.correction())]
-
-
-class RungeKutta:
-    def __init__(self, fs, init_values, start=0, end=math.pi):
-        self.fs = fs
-        self.init_values = init_values
-        self.start = start
-        self.end = end
-
-    def solve(self):
-        ys = self.init_values
-        while True:
-            state = RungeKuttaState(self.start, self.init_values, self.fs)
-            break
 
 
 ButcherTable = namedtuple('ButcherTable', ['c', 'a', 'b'])
 
 
-def helper_table(c2):
-    c = [0, c2]
-    a = [[0, 0], [c2, 0]]
-    b = [1 / (2*c2), 1 - 1 / (2*c2)]
-    return ButcherTable(c, a, b)
+def init_step(system: System, init_values, start, end,  order, error):
+    delta = (1 / max(abs(start), abs(end)))**(order+1) + norm(system.apply(start, *init_values))
+    return (error / delta)**(1 / (order+1))
 
-if __name__ == '__main__':
-    table = helper_table(1 / 12)
+
+def correction(x, ys, system: System, step, table: ButcherTable):
+    """Compute correction as h*f(x+c*h, y+sum(c_i*k_i))"""
+    kss = []
+    for c, a, b in zip(*table):
+        ks = [[a*k for k in ks] for a, ks in zip(a, kss)]
+        ks = np.sum(ks, axis=0) if ks else []
+        ys = [y + k for y, k in zip_longest(ys, ks, fillvalue=0)]
+        sys = system.apply(x + c*step, *ys)
+        kss.append([b * step * f for f in sys])
+    return np.sum(kss, axis=0)
--- RungeKuttaVarStep.py
+++ RungeKuttaVarStep.py
@@ -3,24 +3,7 @@ import numpy as np
 from collections import namedtuple
 from numpy.linalg import norm
 from itertools import zip_longest
-
-
-class System:
-    """Evaluate right hand side of a diff equation system"""
-    def __init__(self, right_hand_side):
-        self.rhs = right_hand_side
-
-    def apply(self, *args):
-        # print(args)
-        return [f(*args) for f in self.rhs]
-
-
-ButcherTable = namedtuple('ButcherTable', ['c', 'a', 'b'])
-
-
-def init_step(system: System, init_values, start, end,  order, error):
-    delta = (1 / max(abs(start), abs(end)))**(order+1) + norm(system.apply(start, *init_values))
-    return (error / delta)**(1 / (order+1))
+from RungeKutta import System, ButcherTable, init_step, correction
 
 
 def solve(system: System, init_values, table: ButcherTable, start=0, end=math.pi, order=2, error=10**-6):
@@ -66,18 +49,6 @@ def solver(x, ys, step, system: System, table: ButcherTable, end, order, error):
         if x >= end:
             break
     return ys
-
-
-def correction(x, ys, system: System, step, table: ButcherTable):
-    """Compute correction as h*f(x+c*h, y+sum(c_i*k_i))"""
-    kss = []
-    for c, a, b in zip(*table):
-        ks = [[a*k for k in ks] for a, ks in zip(a, kss)]
-        ks = np.sum(ks, axis=0) if ks else []
-        ys = [y + k for y, k in zip_longest(ys, ks, fillvalue=0)]
-        sys = system.apply(x + c*step, *ys)
-        kss.append([b * step * f for f in sys])
-    return [sum(k) for k in kss]
 
 
 def helper_table(c2):
